bb05a47708112f342e1451616c9699c908441869
kookong2 <hy010309@naver.com>
2024-03-15 00:59:09 +0900

    backend docker 설정 완료
    스프링은 로컬, 데이터 베이스는 docker에서 실행되도록 설정 완료

--- backend/src/main/java/org/dodam/BackendApplication.java
+++ backend/src/main/java/org/dodam/BackendApplication.java
@@ -1,4 +1,4 @@
-package dodam.backend;
+package org.dodam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/org/dodam/BackendApplicationTests.java
+++ backend/src/test/java/org/dodam/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package dodam.backend;
+package org.dodam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
