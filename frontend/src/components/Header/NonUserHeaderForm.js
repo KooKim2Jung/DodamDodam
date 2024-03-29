f2f076bbc4f56ddd42f26140b3409727af68e68b
GAEUN JEONG <rkdms357@naver.com>
2024-03-29 23:15:10 +0900

    프론트 폴더 파일 정리 및 이름 변경

--- frontend/src/components/Header/NonUserHeaderForm.js
+++ frontend/src/components/Header/NonUserHeaderForm.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './NonUserHeaderForm.css';
+import './HeaderForm.css';
 
 const NonUserHeaderForm = () => {
     return (
         <div className='header-form'>
             <header>
-                <h1 className='title'><img className='flower' src='꽃.png'></img>도담도담<img className='flower' src='꽃.png'></img></h1>
+                <h1 className='title'><img className='flower' src='./image/title_flower.png'>
+                    </img><a href="/">도담도담</a><img className='flower' src='./image/title_flower.png'></img></h1>
                 <nav className='navigation'>
-                    <a href="/LoginForm">로그인</a>
-                    <a href="/SignupForm">회원가입</a>
+                    <a href="/LoginPage">로그인</a>
+                    <a href="/SignupPage">회원가입</a>
                 </nav>
             </header>
         </div>
--- frontend/src/components/Header/UserHeaderForm.js
+++ frontend/src/components/Header/UserHeaderForm.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import './UserHeaderForm.css';
+import './HeaderForm.css';
 
 const UserHeaderForm = () => {
     return (
         <div className='header-form'>
             <header>
-                <h1 className='title'><img className='flower' src='꽃.png'></img>도담도담<img className='flower' src='꽃.png'></img></h1>
+                <h1 className='title'><img className='flower' src='./image/title_flower.png'>
+                    </img><a href="/">도담도담</a><img className='flower' src='./image/title_flower.png'></img></h1>
                 <nav className='navigation'>
-                    <a href="/LoginForm">로그아웃</a>
+                    <a href="/LoginPage">로그아웃</a>
                     <a href="#">내 정보</a>
                 </nav>
             </header>
