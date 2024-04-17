8a16e0a55641e6c70cd614135a6aa7964d041129
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-04-17 14:15:00 +0900

    Merge branch 'develop' of https://github.com/KooKim2Jung/DodamDodam into develop

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,16 +1,14 @@
-# 아무 예제입니다.
 from fastapi import FastAPI
-from typing import Optional
+from dotenv import load_dotenv
+import openai
+import os
+
+from conversations.routes import router
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
 
-@app.get("/users/")
-def read_user(user_id: int, q: Optional[str] = None, skip: int = 0, limit: int = 10):
-    return {"user_id": user_id, "q": q, "skip": skip, "limit": limit}
+app.include_router(router)
