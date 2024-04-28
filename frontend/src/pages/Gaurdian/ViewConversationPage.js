282f6b21e06b12a1d887cc506a1abaa38ab09ff8
GAEUN JEONG <rkdms357@naver.com>
2024-04-28 13:40:06 +0900

    도담이 설정 페이지 퍼블리싱

--- frontend/src/pages/Gaurdian/ViewConversationPage.js
+++ frontend/src/pages/Gaurdian/ViewConversationPage.js
@@ -5,38 +5,38 @@ const ViewConversationPage = () => {
     const dodam = [
         {
             dodam_circle: "./image/dodam_circle.png",
-            text: "선재 배고파.",
+            text: "선재야 배 안고파?.",
         },
         {
             dodam_circle: "./image/dodam_circle.png",
-            text: "선재 배불러.",
+            text: "선재야 엄마가 밥 먹으래.",
         },
         {
             dodam_circle: "./image/dodam_circle.png",
-            text: "선재 졸려.",
+            text: "선재야 엄마가 먼저 자래.",
         },
     ];
 
-    const gaurdian = [
-        { text: "선재야 냉장고에 돈까스 있으니까 전자레인지에 데워서 먼저 밥 먹어" },
-        { text: "선재야 밥 맛있게 먹었어?" },
-        { text: "선재야 엄마 좀 늦을거 같아서 먼저 씻고 자" },
+    const ward = [
+        { text: "도담아 나 심심해" },
+        { text: "도담아 놀자" },
+        { text: "도담아 엄마 안와?" },
     ];
 
     return (
-        <div className='flex flex-col h-screen w-screen ml-[500px]'>
+        <div className='flex flex-col h-screen w-screen pl-[240px]'>
             <AsideForm/>
-            <div className='relative left-1 top-28'>
-                <div className='relative left-5 text-left text-2xl'>2024/03/14</div>
+            <div className='pt-28 pl-5'>
+                <div className='mb-3 text-left text-2xl'>2024/03/14</div>
                 {dodam.map((dodam, index) => (
-                    <div key={index} className='flex m-5'>
-                        <img className='w-28 h-[112px] z-10' src={dodam.dodam_circle} />
+                    <div key={index} className='flex mb-2 items-center'>
+                        <img className='w-[90px] h-[95px] z-10' src={dodam.dodam_circle} />
                         <div className='m-5 p-5 bg-white border-1 rounded-[20px] border-black text-middle-size shadow-[3px_4px_1px_#a5996e]'>{dodam.text}</div>
                     </div>
                 ))}
-                {gaurdian.map((gaurdian, index) => (
-                    <div key={index} className='flex justify-end mr-64'>
-                        <div className='m-5 p-5 bg-secondary border-2 rounded-[20px] border-black text-middle-size shadow-[3px_4px_1px_#a5996e]'>{gaurdian.text}</div>
+                {ward.map((ward, index) => (
+                    <div key={index} className='flex justify-end'>
+                        <div className='m-5 p-5 bg-secondary border-2 rounded-[20px] border-black text-middle-size shadow-[3px_4px_1px_#a5996e]'>{ward.text}</div>
                     </div>
                 ))}
             </div>
