4f6077620e0edc5b4387dfd7f0d06bc124ec8858
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-04-30 20:43:21 +0900

    [TEST] mysql 연동 테스트를 위한 api 생성

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import openai
 import os
-
+from test.sql_routes import router as sql_router
 from conversations.routes import router as conversations_router
 from s3_connection import router as s3_router
 
@@ -15,3 +15,4 @@ app = FastAPI()
 
 app.include_router(conversations_router)
 app.include_router(s3_router)
+app.include_router(sql_router)
