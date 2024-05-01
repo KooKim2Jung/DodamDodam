12bdf37aa0d97a7c4b5698963d50af9a2a36767a
kookong2 <hy010309@naver.com>
2024-05-01 22:48:36 +0900

    docker fastapi까지 연동완료
    springboot는 이슈가 생겨 연동이 안되고 있음

--- backend/src/main/java/org/dodam/BackendApplication.java
+++ backend/src/main/java/org/dodam/BackendApplication.java
@@ -11,15 +11,15 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 
-		// .env 파일에서 환경 변수를 읽어와 시스템 프로퍼티로 설정
-		Dotenv dotenv = Dotenv.load();
-		Properties props = System.getProperties();
-
-		dotenv.entries().forEach(entry -> {
-			props.setProperty(entry.getKey(), entry.getValue());
-		});
-
-		SpringApplication.run(BackendApplication.class, args);
+//		// .env 파일에서 환경 변수를 읽어와 시스템 프로퍼티로 설정
+//		Dotenv dotenv = Dotenv.load();
+//		Properties props = System.getProperties();
+//
+//		dotenv.entries().forEach(entry -> {
+//			props.setProperty(entry.getKey(), entry.getValue());
+//		});
+//
+//		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
