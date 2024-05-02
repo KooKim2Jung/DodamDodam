4d3a67546da4f7cdcb36c98adb54a215e1fb9b7a
kookong2 <hy010309@naver.com>
2024-05-02 12:59:25 +0900

    docker springboot 연결 보류
    fastapi는 연결 해둠

--- backend/src/main/java/org/dodam/BackendApplication.java
+++ backend/src/main/java/org/dodam/BackendApplication.java
@@ -2,7 +2,7 @@ package org.dodam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.Properties;
 
@@ -11,13 +11,13 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 
-//		// .env 파일에서 환경 변수를 읽어와 시스템 프로퍼티로 설정
-//		Dotenv dotenv = Dotenv.load();
-//		Properties props = System.getProperties();
-//
-//		dotenv.entries().forEach(entry -> {
-//			props.setProperty(entry.getKey(), entry.getValue());
-//		});
+		// .env 파일에서 환경 변수를 읽어와 시스템 프로퍼티로 설정
+		Dotenv dotenv = Dotenv.load();
+		Properties props = System.getProperties();
+
+		dotenv.entries().forEach(entry -> {
+			props.setProperty(entry.getKey(), entry.getValue());
+		});
 
 		SpringApplication.run(BackendApplication.class, args);
 	}
--- backend/src/main/java/org/dodam/config/AppConfig.java
+++ backend/src/main/java/org/dodam/config/AppConfig.java
@@ -1,17 +1,15 @@
 package org.dodam.config;
 
-//import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
-//    @Bean
-//    public Dotenv dotenv() {
-//        return Dotenv.configure()
-//                .directory("../") // 이 경로를 상위 폴더로 설정합니다.
-//                .filename(".env") // 명시적으로 파일 이름을 지정할 수 있습니다.
-//                .load();
-//    }
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.load();
+    }
+
 }
