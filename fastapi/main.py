51b6f2cd9e463a330944492ae88526d51b46f913
kookong2 <hy010309@naver.com>
2024-05-02 16:58:24 +0900

    docker fastapi 연동완료

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,14 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
+
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import openai
 import os
+from starlette.requests import Request
+from starlette.responses import Response
+
 
 from conversations.routes import router as conversations_router
 from s3_connection import router as s3_router
@@ -12,6 +19,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(conversations_router)
 app.include_router(s3_router)
