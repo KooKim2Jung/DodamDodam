e4bdcf501802d0379bd556ee6ee9586a3f531b80
GAEUN JEONG <rkdms357@naver.com>
2024-06-05 14:54:57 +0900

    Update ViewEmotionAnalysisPage.js

--- frontend/src/pages/Gaurdian/ViewEmotionAnalysis/ViewEmotionAnalysisPage.js
+++ frontend/src/pages/Gaurdian/ViewEmotionAnalysis/ViewEmotionAnalysisPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import AsideForm from '../../../components/Aside/AsideForm';
+import EmotionAnalysisBoard from '../../../components/EmotionAnalysis/EmotionAnalysisBoard';
+import Calender from '../../../components/EmotionAnalysis/Calender';
 
 const ViewEmotionAnalysisPage = () => {
     return (
-        <div>
+        <div className='flex flex-col h-screen w-screen pl-[240px] pr-10'>
             <AsideForm/>
+            <div className='pt-28 pl-5 relative h-full'>
+                <div className='flex justify-between text-2xl'>
+                    <Calender/>
+                </div>
+            </div>
         </div>
     );
 };
