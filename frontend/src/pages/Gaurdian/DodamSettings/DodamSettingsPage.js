928b304ecb8e90e3f9f9488d058244383fd2ab63
GAEUN JEONG <rkdms357@naver.com>
2024-06-05 15:18:39 +0900

    목소리 이름, API 코드 수정

--- frontend/src/pages/Gaurdian/DodamSettings/DodamSettingsPage.js
+++ frontend/src/pages/Gaurdian/DodamSettings/DodamSettingsPage.js
@@ -3,28 +3,28 @@ import AsideForm from '../../../components/Aside/AsideForm';
 import api from '../../../services/Api';
 
 const DodamSettingsPage = () => {
-    const [voice, setVoice] = useState('하준');
+    const [voice, setVoice] = useState('다정');
     const voices = [
         { name: "다정", }, //하준
         { name: "씩씩", }, //다인
         { name: "활발", }, //야옹
-        { name: "명량", }, //가람
+        { name: "명랑", }, //가람
     ];
 
     // 서버에서 현재 목소리 설정을 가져오는 함수
     const fetchVoiceSetting = async () => {
         try {
             const response = await api.get('/v1/setting');
-            return response.data;  // { voice: '...', speech: '...' } 형태로 반환
+            return response.data;  // { voice: '...' } 형태로 반환
         } catch (error) {
             console.error('Error fetching voice setting:', error);
         }
     };
 
     // 목소리 설정을 서버에 저장하는 함수
-    const saveVoiceSetting = async (voice, speech) => {
+    const saveVoiceSetting = async (voice) => {
         try {
-            const response = await api.post('/v1/setting', { voice, speech });
+            const response = await api.post('/v1/setting', { voice });
             alert(response.data);  // 서버로부터의 응답 메시지를 alert로 표시
         } catch (error) {
             console.error('Error saving voice setting:', error);
