3f79c539955ef39c5c59e8d3cf3c11f86b1b586a
GAEUN JEONG <rkdms357@naver.com>
2024-06-05 16:47:43 +0900

    도담이 기본 인삿말 음성 추가

--- frontend/src/pages/Gaurdian/DodamSettings/DodamSettingsPage.js
+++ frontend/src/pages/Gaurdian/DodamSettings/DodamSettingsPage.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect, useRef }  from 'react';
 import AsideForm from '../../../components/Aside/AsideForm';
 import api from '../../../services/Api';
 
 const DodamSettingsPage = () => {
+    const audioRef = useRef(null);
     const [voice, setVoice] = useState('다정');
     const voices = [
-        { name: "다정", }, //하준
-        { name: "씩씩", }, //다인
-        { name: "활발", }, //야옹
-        { name: "명랑", }, //가람
+        { name: "다정", mp3: '/mp3/Dajeong.mp3'}, //하준
+        { name: "씩씩", mp3: '/mp3/Ssikssik.mp3'}, //다인
+        { name: "활발", mp3: '/mp3/Hwalbal.mp3'}, //야옹
+        { name: "명랑", mp3: '/mp3/Myeonglang.mp3'}, //가람
     ];
 
     // 서버에서 현재 목소리 설정을 가져오는 함수
@@ -50,6 +51,17 @@ const DodamSettingsPage = () => {
         saveVoiceSetting(voice);
     };
 
+    const voiceListening = () => {
+        if (audioRef.current) {
+            audioRef.current.load();
+            audioRef.current.addEventListener('canplaythrough', () => {
+                audioRef.current.play();
+            }, { once: true });
+        }
+    }
+
+    const selectedVoice = voices.find(v => v.name === voice)
+
     return (
         <div className='flex flex-col h-screen w-screen pl-[240px]'>
             <AsideForm/>
@@ -70,12 +82,18 @@ const DodamSettingsPage = () => {
                             </label>
                         ))}<p className='relative top-5 bg-red-30'>
                         <div className='inline-flex items-center'>
-                            <button className='relative -left-3 w-0 h-0 border-t-[20px] border-t-transparent 
-                            border-b-[20px] border-b-transparent border-l-[30px] border-l-borderColor drop-shadow-[1px_4px_1px_#c0c0c0]'></button>
+                            {selectedVoice &&(<><audio controls 
+                            className='relative -left-3 w-0 h-0 border-t-[20px] border-t-transparent 
+                            border-b-[20px] border-b-transparent border-l-[30px] border-l-borderColor drop-shadow-[1px_4px_1px_#c0c0c0]'
+                            type='audio/mp3'
+                            ref={audioRef}
+                            src={`${process.env.PUBLIC_URL}${selectedVoice.mp3}`}
+                            onClick={voiceListening}
+                            />
                             <input 
                                 className='input-box2 w-[600px] p-3 mr-2'
-                                type='text' value='안녕 나는 도담이야. 잘 부탁해.' 
-                            />
+                                type='text' value='안녕 나는 도담이야. 앞으로 잘 부탁해.' 
+                            /></>)}
                         </div>
                         <p><button className='input-box2 relative p-2 w-40 top-5' onClick={voiceSetting}>확인</button></p>
                         </p>
