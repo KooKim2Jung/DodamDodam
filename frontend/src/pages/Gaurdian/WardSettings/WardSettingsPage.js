382879b0f9655a2622fd85fbd16b86e519aaf8d9
GAEUN JEONG <rkdms357@naver.com>
2024-06-27 10:51:00 +0900

    도담이 정보 생성 API 연동 완료

--- frontend/src/pages/Gaurdian/WardSettings/WardSettingsPage.js
+++ frontend/src/pages/Gaurdian/WardSettings/WardSettingsPage.js
@@ -14,7 +14,7 @@ const WardSettingsPage = ({ isEdit, setIsEdit, isWardSetting }) => {
     const [photoUpdated, setPhotoUpdated] = useState(false); // 사용자의 사진 업데이트 여부
     const [previewUrl, setPreviewUrl] = useState(''); // 미리보기 URL 상태
 
-    // 피보호자 정보 생성
+    // 피보호자 정보 생성 & 도담이 정보 생성
     const generateWardSetting = async () => {
         const formData = new FormData();
         if (photoUpdated) {
@@ -27,7 +27,14 @@ const WardSettingsPage = ({ isEdit, setIsEdit, isWardSetting }) => {
 
         try {
             const response = await api.post('/v1/profile', formData);
-            alert(response.data); 
+            alert(response.data);
+            try {
+                const dodamVoiceResponse = await api.post('/v1/setting');
+                alert(dodamVoiceResponse.data);
+            } catch (error) {
+                console.error('도담이 정보 생성 요청 오류', error);
+                alert('도담이 정보 생성을 실패하였습니다.');
+            }
         } catch (error) {
             console.error('피보호자 정보 생성 요청 오류', error);
             alert('피보호자 정보 생성을 실패하였습니다.');
@@ -35,42 +42,42 @@ const WardSettingsPage = ({ isEdit, setIsEdit, isWardSetting }) => {
         }
     };
 
-    // // 피보호자 정보 보기
-    // const getWardSetting = async () => {
-    //     try {
-    //         const response = await api.get('/v1/profile');
-    //         if (response.data) {
-    //             setWardInfo({
-    //                 ...response.data, // API에서 받은 값으로 업데이트
-    //             });
-    //             setPhotoUpdated(false); 
-    //             setPreviewUrl(response.data.photo); // 서버에서 받은 URL을 미리보기로 설정
-    //         }
-    //     } catch (error) {
-    //         console.error('피보호자 정보 보기 요청 오류', error);
-    //     }
-    // };
+    // 피보호자 정보 보기
+    const getWardSetting = async () => {
+        try {
+            const response = await api.get('/v1/profile');
+            if (response.data) {
+                setWardInfo({
+                    ...response.data, // API에서 받은 값으로 업데이트
+                });
+                setPhotoUpdated(false); 
+                setPreviewUrl(response.data.photo); // 서버에서 받은 URL을 미리보기로 설정
+            }
+        } catch (error) {
+            console.error('피보호자 정보 보기 요청 오류', error);
+        }
+    };
 
-    // // 피보호자 정보 수정
-    // const editWardSetting = async () => {
-    //     const formData = new FormData();
-    //     if (photoUpdated) {
-    //         formData.append('photo', wardInfo.photo); // 파일 객체 추가
-    //     } 
-    //     formData.append('name', wardInfo.name);
-    //     formData.append('gender', wardInfo.gender);
-    //     formData.append('age', wardInfo.age);
-    //     formData.append('remark', wardInfo.remark);
+    // 피보호자 정보 수정
+    const editWardSetting = async () => {
+        const formData = new FormData();
+        if (photoUpdated) {
+            formData.append('photo', wardInfo.photo); // 파일 객체 추가
+        } 
+        formData.append('name', wardInfo.name);
+        formData.append('gender', wardInfo.gender);
+        formData.append('age', wardInfo.age);
+        formData.append('remark', wardInfo.remark);
 
-    //     try {
-    //         const response = await api.patch('/v1/profile', formData);
-    //         alert(response.data); 
-    //     } catch (error) {
-    //         console.error('피보호자 정보 수정 요청 오류', error);
-    //         alert('피보호자 정보 수정을 실패하였습니다.');
-    //         setIsEdit(true)
-    //     }
-    // };
+        try {
+            const response = await api.patch('/v1/profile', formData);
+            alert(response.data); 
+        } catch (error) {
+            console.error('피보호자 정보 수정 요청 오류', error);
+            alert('피보호자 정보 수정을 실패하였습니다.');
+            setIsEdit(true)
+        }
+    };
 
     useEffect(() => {
         console.log(isEdit)
