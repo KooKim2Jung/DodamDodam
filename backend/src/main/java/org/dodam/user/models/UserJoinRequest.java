62ef57f5b211d63fa642924a4e0dfe8cc16b1d38
myun1022 <80511975+myun1022@users.noreply.github.com>
2024-06-29 21:41:54 +0900

    [feat] 회원가입 유효성 검사 추가

--- backend/src/main/java/org/dodam/user/models/UserJoinRequest.java
+++ backend/src/main/java/org/dodam/user/models/UserJoinRequest.java
@@ -1,12 +1,25 @@
 package org.dodam.user.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class UserJoinRequest {
+
+    @NotBlank(message = "{email.notBlank}")
+    @Email(message = "{email.invalid}")
     private String email;
+
+    @NotBlank(message = "{password.notBlank}")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "{password.pattern}")
     private String password;
+
+    @NotBlank(message = "{phoneNumber.notBlank}")
+    @Pattern(regexp = "^\\d{11}$", message = "{phoneNumber.pattern}")
     private String phoneNumber;
 }
