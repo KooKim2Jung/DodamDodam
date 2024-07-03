ec570da5012ab70bc4454c187c87950e0d5ea1a6
myun1022 <80511975+myun1022@users.noreply.github.com>
2024-07-03 22:56:45 +0900

    카카오 api 사용자 정보 가져오기

--- fastapi/talk/routes.py
+++ fastapi/talk/routes.py
@@ -11,6 +11,7 @@ access_token = os.getenv("ACCESS_TOKEN")
 refresh_token = os.getenv("REFRESH_TOKEN")
 kakao_api_key = os.getenv("KAKAO_API_KEY")
 redirect_uri = os.getenv("REDIRECT_URI")
+admin_key = os.getenv("ADMIN_KEY")
 authorize_code = "8I33IevZnOerhGajXJWOKULXySKuT_qfnRiHNt9P-nhBvLUX1Ir_9QAAAAQKPXPsAAABkHRSufb-oZq-Jypvmw"
 
 #uuid 안바뀜 but 팀원이 토큰을 발급받아야 보낼 수 있음
@@ -46,6 +47,43 @@ async def get_token():
 
         return response.json()
 
+#사용자 정보 가져오기
+@router.get("/profile")
+async def get_user_profile():
+    url = "https://kapi.kakao.com/v2/user/me"
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=f"Failed to fetch profile: {response.text}")
+
+        return response.json()
+
+#여러 사용자 정보 가져오기
+@router.get("/users")
+async def get_app_users():
+    url = "https://kapi.kakao.com/v2/app/users"
+    headers = {
+        "Authorization": f"KakaoAK {admin_key}"
+    }
+    params = {
+        "target_id_type": "user_id",
+        "target_ids": json.dumps([3601528360, 3602805925, 3606070861, 3602260771])  # JSON 문자열로 변환
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers, params=params)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=f"Failed to fetch app users: {response.text}")
+
+        return response.json()
+
+#친구 목록
 @router.get("/friend")
 async def get_friend():
     url = "https://kapi.kakao.com/v1/api/talk/friends"
@@ -69,9 +107,10 @@ async def send_message():
         "Authorization": f"Bearer {access_token}"
     }
     data = {
-        "receiver_uuids": json.dumps(["uom-iriLvYq5laSTo5elkKKWuou7grqCtYTj", "uo6_irqDu4y_k6GVppKll6GRqYW0hL2FvYq7yg"]),#순서: 진우, 대윤
+        "receiver_uuids": json.dumps(["uom-iriLvYq5laSTo5elkKKWuou7grqCtYTj", "uo6_irqDu4y_k6GVppKll6GRqYW0hL2FvYq7yg", "uo62j72Iu46ikaOSqp2tm6OXu4q6g7uDtIXq"]), #순서: 진우, 대윤, 가은
         #"receiver_uuids": json.dumps(["uom-iriLvYq5laSTo5elkKKWuou7grqCtYTj"]),  # 진우
         #"receiver_uuids": json.dumps(["uo6_irqDu4y_k6GVppKll6GRqYW0hL2FvYq7yg"]),  # 대윤
+        # "receiver_uuids": json.dumps(["uo62j72Iu46ikaOSqp2tm6OXu4q6g7uDtIXq"]),  # 가은
         "template_object": json.dumps({
             "object_type": "text",
             "text": "테스트중입니다",
