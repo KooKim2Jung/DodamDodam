c7ac6b0279e0f891ff6f445d05a058b697f51c61
GAEUN JEONG <rkdms357@naver.com>
2024-08-15 14:47:39 +0900

    Header.js UI 변경

--- frontend/src/components/section/Header/Header.js
+++ frontend/src/components/section/Header/Header.js
@@ -18,28 +18,33 @@ const Header = () => {
     }
 
     return (
-        <header className='fixed top-0 left-0 h-[90px] w-screen shadow-[0px_2px_6px_#a5996e] z-40 bg-primary'>
-            <h1 className='flex items-center justify-center mt-5 text-5xl'>
+        <header className='fixed top-0 left-0 h-[90px] w-screen shadow-[0px_2px_6px_#a5996e] z-40 bg-primary flex items-center justify-center'>
+            <h1 className='absolute left-1/2 transform -translate-x-1/2 flex items-center text-5xl'>
                 <img className='h-10 w-11 mr-1 hidden md:block' src='./images/middle_flower.png'/>
                 <a href="/">도담도담</a>
                 <img className='h-10 w-11 ml-2 hidden md:block' src='./images/middle_flower.png'/>
             </h1>
-            {isLoggedIn ? (<>
-                <div className='absolute top-6 left-[30px] hidden md:left-[875px] md:block'><Toggle /></div>
-                <a className='block md:hidden relative -top-10 left-[375px] pb-[5px]' href="/WardSettingsPage"><FiLock size='30'/></a>
-                <a className='block md:hidden relative -top-[75px] left-[410px] pb-[5px]' href="/WardPage"><FiUnlock size='30'/></a>
-                <nav className='wave absolute -top-10 left-[555px] ml-5'>
-                    <button className='relative text-middle-size pb-[5px] hidden md:block' onClick={handleLogout}>로그아웃</button>
-                    <button className='block md:hidden relative -top-[70px] right-[340px] pb-[5px]' onClick={handleLogout}><FiLogOut size='30'/></button>
-                </nav>
-            </>) : (
-            <nav className='wave absolute -top-10 left-[500px]'>
-                <a className='relative text-middle-size pb-[5px] hidden md:block' href="/SignUpPage"><h2>회원가입</h2></a>
-                <a className='relative text-middle-size pb-[5px] ml-[40px] hidden md:block' href="/LogInPage"><h2>로그인</h2></a>
-                <a className='block md:hidden relative right-[340px] pb-[5px]' href="/SignUpPage"><FiUserPlus size='30'/></a>
-                <a className='block md:hidden relative right-[330px] pb-[5px]' href="/LogInPage"><FiLogIn size='30'/></a>
-            </nav>
-            )}
+            <div className='absolute right-10 flex items-center'>
+                {isLoggedIn ? (<>
+                    <div className='flex items-center wave'>
+                        <div className='relative hidden md:block mr-16'><Toggle /></div>
+                        <a className='block md:hidden relative mr-4 pb-1' href="/WardSettingsPage"><FiLock size='30'/></a>
+                        <a className='block md:hidden relative mr-4 pb-1' href="/WardPage"><FiUnlock size='30'/></a>
+                    </div>
+                    <div className='flex items-center wave'>
+                        <button className='relative text-middle-size hidden md:block mr-6'>도움말</button>
+                        <button className='relative text-middle-size hidden md:block' onClick={handleLogout}>로그아웃</button>
+                        <button className='block md:hidden relative pb-1' onClick={handleLogout}><FiLogOut size='30'/></button>
+                    </div>
+                    </>) : (
+                    <div className='flex items-center wave'>
+                        <a className='relative text-middle-size hidden md:block mr-6' href="/SignUpPage"><h2>회원가입</h2></a>
+                        <a className='relative text-middle-size hidden md:block' href="/LogInPage"><h2>로그인</h2></a>
+                        <a className='block md:hidden relative mr-4 pb-1' href="/SignUpPage"><FiUserPlus size='30'/></a>
+                        <a className='block md:hidden relative pb-1' href="/LogInPage"><FiLogIn size='30'/></a>
+                    </div>
+                )}
+            </div>
         </header>
     );
 };
