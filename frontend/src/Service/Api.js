10bc5bef8f82bd5da64290b94738002daaf03947
GAEUN JEONG <rkdms357@naver.com>
2024-08-22 14:32:24 +0900

    회원가입/로그인  API 호출 및 처리 로직 수정

--- frontend/src/Service/Api.js
+++ frontend/src/Service/Api.js
@@ -5,11 +5,7 @@ const api = axios.create();
 api.interceptors.request.use((config) => {
     const token = sessionStorage.getItem("jwtToken");
     // 요청 URL에 따라 baseURL 동적으로 설정
-    if (config.url.includes('/auth')) {
-        config.baseURL = "http://localhost:8082/api";
-    } else {
-        config.baseURL = "http://localhost:8080/api";
-    }
+    config.baseURL = "http://localhost:8080/api";
 
     // 회원가입 및 로그인 요청일 경우 Authorization 헤더를 추가하지 않음
     if (token && !config.url.endsWith('/auth/join') && !config.url.endsWith('/auth/login')) {
