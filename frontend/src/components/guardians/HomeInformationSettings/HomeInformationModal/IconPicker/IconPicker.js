42357ff6618ce8ade6c745f5254be232b2b4eccd
GAEUN JEONG <rkdms357@naver.com>
2024-08-24 15:23:42 +0900

    UI 수정

--- frontend/src/components/guardians/HomeInformationSettings/HomeInformationModal/IconPicker/IconPicker.js
+++ frontend/src/components/guardians/HomeInformationSettings/HomeInformationModal/IconPicker/IconPicker.js
@@ -17,14 +17,16 @@ const IconPicker = () => {
     return (
         <div>
             <Modal
-                overlayClassName='fixed -top-20 left-36 z-20 inset-0 flex justify-center items-center'
+                overlayClassName='fixed top-20 left-36 z-20 inset-0 flex justify-center items-center'
                 isOpen={isIconPickerOpen}
-                className='w-[200px] h-[200px] relative flex-col justify-center bg-primary rounded-[60px] shadow-[4px_6px_2px_#a5996e]'
+                className='relative flex-col justify-center bg-primary rounded-[40px] shadow-[4px_6px_2px_#a5996e]'
             >
-            <div><EmojiPicker/></div>
             <div className='flex justify-end mt-3 mr-6'>
                 <button className='w-8 h-8 bg-primary border-2 text-2xl items-center 
                 justify-center flex rounded-[50px] border-white hover:scale-110' onClick={closeIconPicker}><FiX /></button>
+            </div>
+            <div className='mx-5 mt-2 mb-5 border-none'>
+                <EmojiPicker emojiStyle='native' searchDisabled={true} autoFocusSearch={false} previewConfig={{ showPreview: false }} width={300} height={300}/>
             </div>
             </Modal>
             <button className='p-2 mr-2 rounded-[50px] border-2 border-black hover:scale-110' onClick={openIconPicker}><FiSmile /></button>
