4b45c24ebca16da408f0c6f452ad2194f3eea618
GAEUN JEONG <rkdms357@naver.com>
2024-08-24 20:52:20 +0900

    로컬스토리지에 이모티콘 저장&수정 가능

--- frontend/src/components/guardians/HomeInformationSettings/HomeInformationFolder.js
+++ frontend/src/components/guardians/HomeInformationSettings/HomeInformationFolder.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import HomeInformation from './HomeInformationModal/HomeInformation';
+import { AppContext } from '../../../App';
 
 const HomeInformationFolder = ({ folder, deleteFolder }) => {
+    const { isEmojiSelected, setIsEmojiSelected } = useContext(AppContext);
+
     const [isFolderOpen, setIsFolderOpen] = useState(false);
+    const [newFolderEmoji, setNewFolderEmoji] = useState('');
 
     const openFolder = () => {
         setIsFolderOpen(true);
     }
 
+    useEffect(() => {
+        if (isEmojiSelected) {
+            const savedEmojis = JSON.parse(localStorage.getItem('selectedEmojis')) || {};
+            const folderEmoji = savedEmojis[folder.id];
+            setNewFolderEmoji(folderEmoji);
+        } setIsEmojiSelected(false);
+    }, [isEmojiSelected, folder.id]);
+
     return (
         (folder && 
         <div className='flex mx-9'>
-            <div className='h-44 w-44 bg-tertiay rounded-[70px] flex items-center justify-center shadow-[2px_4px_1px_#a5996e] cursor-pointer'
-                onClick={openFolder}>
+            <div>
+                <div className='h-44 w-44 bg-tertiay rounded-[70px] flex items-center justify-center shadow-[2px_4px_1px_#a5996e] cursor-pointer'
+                    onClick={openFolder}>
+                    <div className='relative flex items-center justify-center z-1 text-7xl'>{newFolderEmoji}</div>
+                </div>
             </div>
             <HomeInformation 
                 isFolderOpen={isFolderOpen} 
--- frontend/src/components/guardians/HomeInformationSettings/HomeInformationModal/IconPicker/IconPicker.js
+++ frontend/src/components/guardians/HomeInformationSettings/HomeInformationModal/IconPicker/IconPicker.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { FiX, FiSmile } from "react-icons/fi";
 import Modal from 'react-modal';
 import EmojiPicker from 'emoji-picker-react';
+import { AppContext } from '../../../../../App';
 
-const IconPicker = () => {
+const IconPicker = ({ folder }) => {
+    const { setIsEmojiSelected } = useContext(AppContext);
+
     const [isIconPickerOpen, setIsIconPickerOpen] = useState(false);
 
     const openIconPicker = () => {
@@ -12,6 +15,14 @@ const IconPicker = () => {
 
     const closeIconPicker = () => {
         setIsIconPickerOpen(false);
+    }
+
+    const handleEmojiClick = (e, folderId) => {
+        const selectedEmojis = JSON.parse(localStorage.getItem('selectedEmojis')) || {};
+        selectedEmojis[folderId] = e.emoji;
+        localStorage.setItem('selectedEmojis', JSON.stringify(selectedEmojis));
+        setIsEmojiSelected(true);
+        closeIconPicker();
     }
 
     return (
@@ -25,8 +36,15 @@ const IconPicker = () => {
                 <button className='w-8 h-8 bg-primary border-2 text-2xl items-center 
                 justify-center flex rounded-[50px] border-white hover:scale-110' onClick={closeIconPicker}><FiX /></button>
             </div>
-            <div className='mx-5 mt-2 mb-5 border-none'>
-                <EmojiPicker emojiStyle='native' searchDisabled={true} autoFocusSearch={false} previewConfig={{ showPreview: false }} width={300} height={300}/>
+            <div className='mx-5 mt-2 mb-5'>
+                <EmojiPicker 
+                    emojiStyle='native' 
+                    searchDisabled={true} 
+                    autoFocusSearch={false} 
+                    previewConfig={{ showPreview: false }} 
+                    width={300} height={300}
+                    onEmojiClick={(e) => handleEmojiClick(e, folder.id)}
+                />
             </div>
             </Modal>
             <button className='p-2 mr-2 rounded-[50px] border-2 border-black hover:scale-110' onClick={openIconPicker}><FiSmile /></button>
