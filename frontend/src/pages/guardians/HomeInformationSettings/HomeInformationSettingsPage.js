39ffdce183086d92cbf90a0da4cf8152db5a2453
GAEUN JEONG <rkdms357@naver.com>
2024-08-25 20:34:51 +0900

    폴더기능 삭제

--- frontend/src/pages/guardians/HomeInformationSettings/HomeInformationSettingsPage.js
+++ frontend/src/pages/guardians/HomeInformationSettings/HomeInformationSettingsPage.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
 import Aside from '../../../components/section/Aside/Aside';
-import HomeInformationBoard from '../../../components/guardians/HomeInformationSettings/HomeInformationBoard';
 import { AppContext } from '../../../App';
 import Guardian from '../../../components/guardians/Guardian/Guardian';
+import HomeInformation from '../../../components/guardians/HomeInformationSettings/HomeInformationModal/HomeInformation';
 
 const HomeInformationSettingsPage = () => {
     const { isGuardian, setIsGuardian, isWardSetting } = useContext(AppContext);
@@ -12,7 +12,7 @@ const HomeInformationSettingsPage = () => {
             <Aside/>
             <h2 className='absolute top-28 left-[265px]'>집 정보 설정</h2>
             <div className='flex justify-center pl-60 mt-36'>
-                <HomeInformationBoard />
+                <HomeInformation/>
             </div>
             <Guardian isGuardian={isGuardian} setIsGuardian={setIsGuardian} isWardSetting={isWardSetting}/>
         </div>
