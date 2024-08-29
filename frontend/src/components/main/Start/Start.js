9cc712d858b77ee754344b1db42e0c77ff4d339d
GAEUN JEONG <rkdms357@naver.com>
2024-08-29 14:21:04 +0900

    AppProvider.js 이름 변경

--- frontend/src/components/main/Start/Start.js
+++ frontend/src/components/main/Start/Start.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AppContext } from '../../../App';
+import { AppContext } from '../../../AppProvider';
 
 const Start = () => {
     const { isLoggedIn } = useContext(AppContext);
