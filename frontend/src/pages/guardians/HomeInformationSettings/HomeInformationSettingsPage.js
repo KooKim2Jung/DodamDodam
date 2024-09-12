fba41e09565213f15bbb996f2509508745aa8d1a
GAEUN JEONG <rkdms357@naver.com>
2024-09-12 11:44:00 +0900

    피보호자 설정 반응형 디자인 적용

--- frontend/src/pages/guardians/HomeInformationSettings/HomeInformationSettingsPage.js
+++ frontend/src/pages/guardians/HomeInformationSettings/HomeInformationSettingsPage.js
@@ -82,9 +82,9 @@ const HomeInformationSettingsPage = () => {
         <div className='flex flex-col h-screen w-screen md:pl-[240px] text-2xl'>
             <div className='pt-20 md:pt-28'>
                 <h2 className='text-3xl text-left pl-8'>집 정보 설정</h2>
-                <div className='flex flex-col bg-red-600'>
-                    <div className='flex-grow basis-[90%] bg-tertiay rounded-[60px] shadow-[6px_4px_10px_#a5996e] p-7'>
-                        <div className='overflow-y-auto h-full w-full'>
+                <div className='flex flex-col m-5'>
+                    <div className='bg-primary rounded-[60px] shadow-[6px_4px_10px_#a5996e] p-[3vh] h-[63vh] md:h-[59vh] min-h-[300px]'>
+                        <div className='overflow-y-auto h-[57vh] md:h-[54vh] overflow-x-hidden'>
                             <div className='text-2xl w-full my-3 text-gray-400'>{error}</div>
                             {items.map((item, index) => (
                             <>{isEditing && currentItemIndex === index ? (
@@ -95,7 +95,7 @@ const HomeInformationSettingsPage = () => {
                                     items={items}
                                     index={index}
                                 />) : (
-                                <div className='items-center flex justify-center border-transparent bg-white shadow-[2px_4px_1px_#a5996e] rounded-[50px]' 
+                                <div className='items-center flex justify-center border-transparent bg-white shadow-[2px_4px_1px_#a5996e] rounded-[50px] m-5' 
                                 key={index}>
                                     {item.data}
                                     <button onClick={() => handleEdit(index)} className='p-2 text-2xl rounded-[50px] border-2 mx-2 my-2 border-black hover:scale-110'><FiEdit2 /></button>
@@ -105,7 +105,7 @@ const HomeInformationSettingsPage = () => {
                             ))}
                         </div>
                     </div>
-                    <div className='flex-grow basis-[10%] bg-blue-800'>
+                    <div className='absolute bottom-3 md:left-[250px] right-1 left-0'>
                         <HomeInformationForm addItem={addItem} info={info} setInfo={setInfo}/>
                     </div>
                 </div>
