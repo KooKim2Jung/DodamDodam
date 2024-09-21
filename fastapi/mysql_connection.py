b775ce1e4bc5b0c2aa2c09b072c750bcdb5b2e16
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-09-21 17:57:46 +0900

    env: celery 설정

--- fastapi/mysql_connection.py
+++ fastapi/mysql_connection.py
@@ -1,3 +1,4 @@
+# db.py (기존 파일 이름 유지 또는 새로운 파일로 저장)
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -5,22 +6,27 @@ import os
 from dotenv import load_dotenv
 from typing import Generator
 
-
+# .env 파일 로드
 load_dotenv()
 
+# 환경 변수에서 데이터베이스 연결 정보 가져오기
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_PORT = os.getenv("MYSQL_PORT")
 MYSQL_DB = os.getenv("MYSQL_DATABASE")
 
+# SQLAlchemy 데이터베이스 URL 설정
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
+# 엔진 및 세션 설정
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# 모델 베이스 클래스
 Base = declarative_base()
 
+# 데이터베이스 세션 생성 및 반환 함수
 def get_db() -> Generator:
     db = SessionLocal()
     try:
