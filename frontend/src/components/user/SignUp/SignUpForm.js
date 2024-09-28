7697b8317d3a814c3af425eb30b44547ed8405bc
GAEUN JEONG <rkdms357@naver.com>
2024-09-28 09:38:35 +0900

    회원가입 폼 기존 useState, validation 삭제

--- frontend/src/components/user/SignUp/SignUpForm.js
+++ frontend/src/components/user/SignUp/SignUpForm.js
@@ -1,35 +1,24 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-const SignUpForm = ({welcomeUser, setWelcomeUser}) => {
-    const submitWelcomeUser = (e) => {
-        const { name, value } = e.target;
-        setWelcomeUser(welcomeUser => ({
-            ...welcomeUser,
-            [name]: value
-        }))
-    }
+const SignUpForm = () => {
+
     return (
         <div>
             <input className='input-box'
                 type="text"
                 name='email'
-                value={welcomeUser.email}
                 placeholder='이메일'
-                onChange={submitWelcomeUser}
             />
             <input className='input-box'
                 type="password"
                 name='password'
-                value={welcomeUser.password}
                 placeholder='비밀번호(8~16자 영문자, 특수문자 포함)'
-                onChange={submitWelcomeUser}
             />
             <input className='input-box'
                 type="number"
                 name='phoneNumber'
-                value={welcomeUser.phoneNumber}
                 placeholder='전화번호(11자리, -제외)'
-                onChange={submitWelcomeUser}
             />
         </div>
     );
--- frontend/src/pages/user/SignUp/SignUpPage.js
+++ frontend/src/pages/user/SignUp/SignUpPage.js
@@ -2,16 +2,9 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../../Service/Api';
 import SignUpForm from '../../../components/user/SignUp/SignUpForm';
-import SignUpCheck from '../../../components/user/SignUp/SignUpCheck';
 
 const SignUpPage = () => {
-    const [welcomeUser, setWelcomeUser] = useState({
-        email: '',
-        password: '',
-        phoneNumber: '',
-    })
     const navigate = useNavigate();
-
     const [errorMessage, setErrorMessage] = useState('')    
 
     const resetForm = () => {
@@ -22,57 +15,35 @@ const SignUpPage = () => {
         navigate('/LogInPage');
     }
 
-    const submitSignUp  = async (event) => {
-        event.preventDefault();
-        if (validateForm()) {
-            try {
-                const response = await api.post('/v1/auth/join', {
-                    email: welcomeUser.email,
-                    password: welcomeUser.password,
-                    phone_number: welcomeUser.phoneNumber
-                });
-                alert(response.data.message);
-                handleLogIn();
-            } catch (signUpError) {
-                console.log("회원가입 요청 오류", signUpError);
-                const message = signUpError.response.data.detail;
-                setErrorMessage(message);
-            }
-        }
-    };
-
-    const validateForm = () => {
-        resetForm();
-
-        let isValid = true;
-        if (!welcomeUser.email) {
-            setErrorMessage('이메일은 필수 입력 항목입니다.')
-            isValid = false;
-        } 
-        if (welcomeUser.email && !welcomeUser.password) {
-            setErrorMessage('비밀번호는 필수 입력 항목입니다.')
-            isValid = false;
-        }
-        if (welcomeUser.email && welcomeUser.password && !welcomeUser.phoneNumber) {
-            setErrorMessage('전화번호는 필수 입력 항목입니다.')
-            isValid = false;
-        }
-        return isValid;
-    }
+    // const submitSignUp  = async (event) => {
+    //     event.preventDefault();
+    //     if (validateForm()) {
+    //         try {
+    //             const response = await api.post('/v1/auth/join', {
+    //                 email: welcomeUser.email,
+    //                 password: welcomeUser.password,
+    //                 phone_number: welcomeUser.phoneNumber
+    //             });
+    //             alert(response.data.message);
+    //             handleLogIn();
+    //         } catch (signUpError) {
+    //             console.log("회원가입 요청 오류", signUpError);
+    //             const message = signUpError.response.data.detail;
+    //             setErrorMessage(message);
+    //         }
+    //     }
+    // };
 
     return (
-        <form onSubmit={submitSignUp}>
-            <div className='relative flex justify-center items-center mt-[120px] mb-[25px] 
-            w-[560px] h-[680px] rounded-[50px] shadow-[6px_4px_10px_#a5996e]'>
-                <img src='/images/dodam_signup.png' className='w-[440px] h-[600px]'/>
-                <div className='absolute mt-40 w-[360px] z-10'>
-                    <h1 className='text-basic-size'>회원가입</h1>
-                    <SignUpForm welcomeUser={welcomeUser} setWelcomeUser={setWelcomeUser}/>
-                    <SignUpCheck errorMessage={errorMessage}/>
-                    <button className='btn -mt-4' type='submit'>가입하기</button>
-                </div>
+        <div className='relative flex justify-center items-center mt-[120px] mb-[25px] 
+        w-[560px] h-[680px] rounded-[50px] shadow-[6px_4px_10px_#a5996e]'>
+            <img src='/images/dodam_signup.png' className='w-[440px] h-[600px]'/>
+            <div className='absolute mt-40 w-[360px] z-10'>
+                <h1 className='text-basic-size'>회원가입</h1>
+                <SignUpForm />
+                <button className='btn -mt-4' type='submit'>가입하기</button>
             </div>
-        </form>
+        </div>
     );
 };
 
