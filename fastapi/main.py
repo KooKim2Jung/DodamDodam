38e0dfb827935b4638fe0d3e22284a396bf448ea
myun1022 <80511975+myun1022@users.noreply.github.com>
2024-10-05 00:33:42 +0900

    [feat]nginx 스웨거 설정 추가

--- fastapi/main.py
+++ fastapi/main.py
@@ -42,7 +42,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # 허용할 도메인 목록
+    allow_origins=["*"],  # 허용할 도메인 목록
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 HTTP 헤더 허용
